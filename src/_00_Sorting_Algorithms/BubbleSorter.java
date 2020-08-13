bbe4caee6698665d49d25020b195a7fd060e52b7
Derek <Derek@MSI>
2020-08-13 16:34:18 -0700

    Bubble sorter, selection sorter

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -10,7 +10,18 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
+			for (int i = 0; i < array.length - 1; i++) {
+				for (int j = i + 1; j < array.length; j++) {
+					if (array[i] > array[j]) {
+						int temp = array[j];
+						array[j] = array[i];
+						array[i] = temp;
+					}
+				}
+				display.updateDisplay();
+			}
 		
+	
 	}
 	
 }
